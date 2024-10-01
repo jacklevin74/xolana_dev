ddd31fb615c1b703b1bf54fdcc59b9ba8adefc27
Ubuntu <ubuntu@c2-large-x86-lax-1.maas>
2024-10-01 19:19:24 +0000

    Revert accounts-db/src/blockhash_queue.rs to commit aeb3a2e15c96d23a53d03ef3f88ee82a52e5c47e

--- accounts-db/src/blockhash_queue.rs
+++ accounts-db/src/blockhash_queue.rs
@@ -80,8 +80,7 @@ impl BlockhashQueue {
         self.ages.insert(
             *hash,
             HashAge {
-                //fee_calculator: FeeCalculator::new(lamports_per_signature),
-                fee_calculator: FeeCalculator::new(50),
+                fee_calculator: FeeCalculator::new(lamports_per_signature),
                 hash_index: 0,
                 timestamp: timestamp(),
             },
@@ -105,7 +104,7 @@ impl BlockhashQueue {
         self.ages.insert(
             *hash,
             HashAge {
-                fee_calculator: FeeCalculator::new(50),
+                fee_calculator: FeeCalculator::new(lamports_per_signature),
                 hash_index: self.last_hash_index,
                 timestamp: timestamp(),
             },
@@ -121,7 +120,7 @@ impl BlockhashQueue {
     #[allow(deprecated)]
     pub fn get_recent_blockhashes(&self) -> impl Iterator<Item = recent_blockhashes::IterItem> {
         (self.ages).iter().map(|(k, v)| {
-            recent_blockhashes::IterItem(v.hash_index, k, 50)
+            recent_blockhashes::IterItem(v.hash_index, k, v.fee_calculator.lamports_per_signature)
         })
     }
 
